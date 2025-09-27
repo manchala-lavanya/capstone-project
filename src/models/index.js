3e3e245bae8c5b6b6814b44f41af647b199d17b3
Manchala Lavanya <manchala.lavanya@cognizant.com>
2025-09-27 15:45:35 +0530

    Update index.js

--- src/models/index.js
+++ src/models/index.js
@@ -1,10 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
 const db = {};
 
 db.mongoose = mongoose;
-
-db.user = require("./book.model");
+db.user = require("./user.model");
+db.book = require("./book.model");
+db.review = require("./review.model");
 
 module.exports = db;
