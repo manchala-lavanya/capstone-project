40f73ff484ca3c86e563cdfe5f5956f93792e4f1
Manchala Lavanya <manchala.lavanya@cognizant.com>
2025-09-28 12:30:46 +0530

    Update auth.routes.js

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,4 +1,6 @@
 const authController = require("../controllers/auth.controller");
+const verifyToken = require('../middleware/auth.middleware');
+const verifyRole = require('../middleware/role.middleware');
 
 module.exports = function(app) {
   // Register user
@@ -6,4 +8,7 @@ module.exports = function(app) {
 
   // Login user
   app.post("/api/login", authController.login);
+
+  // Register admin - Only admins can create another admin
+  app.post("/api/admin/register", [verifyToken, verifyRole('admin')], authController.registerAdmin);
 };
